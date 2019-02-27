b77cf00e511a87d031334b7be21ce8158ad08418
Elan Feingold <elan@plex.tv>
2019-02-27 09:39:20 -1000

    Update

--- download-files.sh
+++ download-files.sh
@@ -8,11 +8,11 @@ rm -rf libopus-src/*
 rm -rf libopusfile-src/*
 rm -rf libogg-src/*
 
-curl -0 http://downloads.xiph.org/releases/opus/opus-1.1.3.tar.gz | tar -zx --strip 1 -C libopus-src/
-curl -0 http://downloads.xiph.org/releases/opus/opusfile-0.8.tar.gz | tar -zx --strip 1 -C libopusfile-src/
-curl -0 http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz | tar -zx --strip 1 -C libogg-src/
+curl -0 -L http://downloads.xiph.org/releases/opus/opus-1.2.1.tar.gz | tar zx --strip 1 -C libopus-src/
+curl -0 -L http://downloads.xiph.org/releases/opus/opusfile-0.10.tar.gz | tar zx --strip 1 -C libopusfile-src/
+curl -0 -L http://downloads.xiph.org/releases/ogg/libogg-1.3.3.tar.gz | tar zx --strip 1 -C libogg-src/
 
-patch -p0 < opus-include.patch
+#patch -p0 < opus-include.patch
 
 ##
 ## this is how the opus-include.patch got made
